ac4a4779a901a85dc7bf2768739f734f8f5ecce4
revaxl <revaxl09@gmail.com>
2022-02-15 21:44:01 +0900

    create factory class for Event model

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -2,17 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
+use App\Models\Event;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class UserFactory extends Factory
+class EventFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = User::class;
+    protected $model = Event::class;
 
     /**
      * Define the model's default state.
@@ -23,7 +23,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'description' => $this->faker->paragraph(),
+            'date' => $this->faker->date(),
+            'availability' => $this->faker->randomDigitNotZero() * 10
         ];
     }
 }
