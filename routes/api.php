adea367331757f6899d32731bcaf40cadb64b8e2
revaxl <revaxl09@gmail.com>
2022-02-18 12:12:13 +0900

    change the route file from web to api

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,10 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
-$router->get('/', 'EventController@index');
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->group(['prefix' => 'events'], function () use ($router) {
+        $router->get('/', 'EventController@index');
+        $router->post('{eventId}/reserve', 'EventController@reserve');
+        $router->post('{eventId}/cancel', 'EventController@cancel');
+    });
+});
